3ee17559b401228b8f5ac5b58b012ba100bc2776
huseynalieff <huseyn.alieff@gmail.com>
2018-08-22 21:25:37 +0300

    Update yaz.js

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
+  if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`yaz` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
   message.delete();
@@ -11,7 +18,7 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['say', 'söyle'],
-  permLevel: 0
+  permLevel: 1
 };
 
 exports.help = {
