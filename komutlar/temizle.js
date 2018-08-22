a6ccc1f486ad3454974bcd059d222ec9740b1b53
huseynalieff <huseyn.alieff@gmail.com>
2018-08-22 21:21:29 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 
 
 exports.run = function(client, message) {
+	if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', '`temizle` adlı komutu özel mesajlarda kullanamazsın.')
+  return message.author.sendEmbed(ozelmesajuyari); }
 message.channel.bulkDelete(20);
 message.channel.send("20 mesaj sildim :muscle: ").then(msg => {
 	msg.delete(5000)
